e3f1b8002c80d3e723dee97cc75424cdee3f7c97
viiku <vivekvikkusingh@gmail.com>
2024-05-23 12:33:31 +0530

    add serializers and use res framework

---
+++ watchlist_app/api/views.py
@@ -0,0 +1,17 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from watchlist_app.models import Movie
+from watchlist_app.api.serializers import MovieSerializer
+# from rest_framework.renderers import JSONRenderer, TemplateHTMLRenderer
+
+@api_view()
+def movie_list(request):
+    movies = Movie.objects.all()
+    serializer = MovieSerializer(movies, many=True)
+    return Response(serializer.data)
+
+@api_view()
+def movie_details(request, pk):
+    movie = Movie.objects.get(pk = pk)
+    serializer = MovieSerializer(movie)
+    return Response(serializer.data)
--- watchlist_app/views.py
+++ watchlist_app/views.py
@@ -1,23 +1,23 @@
-from django.shortcuts import render
-from watchlist_app.models import Movie
-from django.http import JsonResponse
-# Create your views here.
+# from django.shortcuts import render
+# from watchlist_app.models import Movie
+# from django.http import JsonResponse
+# # Create your views here.
 
-def movie_list(request):
-    movies = Movie.objects.all()
-    print(list(movies.values()))
-    data = {
-        'movies': list(movies.values()),
-    }
-    return JsonResponse(data)
+# def movie_list(request):
+#     movies = Movie.objects.all()
+#     print(list(movies.values()))
+#     data = {
+#         'movies': list(movies.values()),
+#     }
+#     return JsonResponse(data)
     
-def movie_details(request,pk):
-    movie = Movie.objects.get(pk=pk)
-    data = {
-        'name': movie.name,
-        'description': movie.description,
-        'active': movie.active,
-    }
-    print(movie)
-    return JsonResponse(data)
+# def movie_details(request,pk):
+#     movie = Movie.objects.get(pk=pk)
+#     data = {
+#         'name': movie.name,
+#         'description': movie.description,
+#         'active': movie.active,
+#     }
+#     print(movie)
+#     return JsonResponse(data)
     
