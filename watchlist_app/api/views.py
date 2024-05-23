d20959e2c7d594627168963f7dbac11168399627
viiku <vivekvikkusingh@gmail.com>
2024-05-23 13:44:30 +0530

    add crud operations

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -1,17 +1,48 @@
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.decorators import api_view
 from watchlist_app.models import Movie
 from watchlist_app.api.serializers import MovieSerializer
 # from rest_framework.renderers import JSONRenderer, TemplateHTMLRenderer
 
-@api_view()
+@api_view(['GET', 'POST'])
 def movie_list(request):
-    movies = Movie.objects.all()
-    serializer = MovieSerializer(movies, many=True)
-    return Response(serializer.data)
 
-@api_view()
+    if request.method == 'GET':
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data)
+    
+    if request.method == 'POST':
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+@api_view(['GET', 'PUT', 'DELETE'])
 def movie_details(request, pk):
-    movie = Movie.objects.get(pk = pk)
-    serializer = MovieSerializer(movie)
-    return Response(serializer.data)
+    
+    if request.method == 'GET':
+        try:
+            movie = Movie.objects.get(pk = pk)
+        except Movie.DoesNotExist:
+            return Response({'Error': 'Movie Not Found'}, status=status.HTTP_404_NOT_FOUND)    
+        
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data)
+    
+    if request.method == 'PUT':
+        movie = Movie.objects.get(pk = pk)
+        serializer = MovieSerializer(movie, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response(serializer.errors)
+    
+    if request.method == 'DELETE':
+        movie = Movie.objects.get(pk = pk)
+        movie.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
