e3117ed7288c8bb7ae225f0dc6e28b9cad1dcf5a
viiku <vivekvikkusingh@gmail.com>
2024-05-27 21:36:56 +0530

    adding models

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -3,8 +3,8 @@ from django.urls import path, include
 from watchlist_app.api.views import WatchListAV, WatchDetailsAV, StreamPlatformAV, StreamPlatformDetailsAV
 
 urlpatterns = [
-    path('list/', WatchListAV.as_view(), name='movie_list'),
-    path('<int:pk>', WatchDetailsAV.as_view(), name='movie_details'),
-    path('stream/', StreamPlatformAV.as_view(), name='stream_platform_list'),
-    path('stream/<int:pk>', StreamPlatformDetailsAV.as_view(), name='stream_platform_details'),
+    path('list/', WatchListAV.as_view(), name='movie-list'),
+    path('<int:pk>', WatchDetailsAV.as_view(), name='movie-detail'),
+    path('stream/', StreamPlatformAV.as_view(), name='stream-platform-list'),
+    path('stream/<int:pk>', StreamPlatformDetailsAV.as_view(), name='stream-platform-detail'),
 ]
--- watchlist_app/models.py
+++ watchlist_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 class StreamPlatform(models.Model):
@@ -12,9 +13,20 @@ class StreamPlatform(models.Model):
 class WatchList(models.Model):
     title = models.CharField(max_length=50)
     storyline = models.CharField(max_length=200)
-    platform = models.ForeignKey(StreamPlatform, on_delete=models.CASCADE, related_name='watchlist')
+    platform = models.ForeignKey(StreamPlatform, on_delete=models.CASCADE, related_name="watchlist")
     active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return self.title
+    
+class Review(models.Model):
+    rating = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    description = models.CharField(max_length=200, null=True)
+    watchList = models.ForeignKey(WatchList, on_delete=models.CASCADE, related_name="reviews")
+    active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return str(self.rating)+ " "+ self.watchList.title
