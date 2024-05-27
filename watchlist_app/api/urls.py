ec692b4cd05342b8b6894ad644dcebd02685ac32
viiku <vivekvikkusingh@gmail.com>
2024-05-27 14:04:25 +0530

    implement class based view

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path, include
-from watchlist_app.api.views import movie_list, movie_details
+# from watchlist_app.api.views import movie_list, movie_details
+from watchlist_app.api.views import MovieListAV, MovieDetailAV
 
 urlpatterns = [
-    path('list/', movie_list, name='movie_list'),
-    path('<int:pk>', movie_details, name='movie_details'),
+    # path('list/', movie_list, name='movie_list'),
+    # path('<int:pk>', movie_details, name='movie_details'),
+    
+    path('list/', MovieListAV.as_view(), name='movie_list'),
+    path('<int:pk>', MovieDetailAV.as_view(), name='movie_details'),
 ]
