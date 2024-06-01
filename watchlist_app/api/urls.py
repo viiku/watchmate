5a7ce34a244a68e87149e719df09edc4d66a6dde
viiku <vivekvikkusingh@gmail.com>
2024-06-01 12:04:13 +0530

    generic api-view

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
 # from watchlist_app.api.views import movie_list, movie_details
-from watchlist_app.api.views import WatchListAV, WatchDetailsAV, StreamPlatformAV, StreamPlatformDetailsAV
+from watchlist_app.api.views import ReviewList, ReviewDetail, WatchListAV, WatchDetailsAV, StreamPlatformAV, StreamPlatformDetailsAV
 
 urlpatterns = [
     path('list/', WatchListAV.as_view(), name='movie-list'),
     path('<int:pk>', WatchDetailsAV.as_view(), name='movie-detail'),
     path('stream/', StreamPlatformAV.as_view(), name='stream-platform-list'),
     path('stream/<int:pk>', StreamPlatformDetailsAV.as_view(), name='stream-platform-detail'),
+    
+    path('review/', ReviewList.as_view(), name='review-list'),
+    path('review/<int:pk>', ReviewDetail.as_view(), name='review-detail'),
 ]
